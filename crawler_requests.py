8820567871ab47971c66877b0b4809f1cc16629e
Derek Chiang <b00611007@gmail.com>
2017-03-12 19:45:55 +0800

    update files

--- crawler_requests.py
+++ crawler_requests.py
@@ -1,8 +1,7 @@
 # encoding = 'utf-8'
 import requests
 
-user = 'rg3'
-user_url = 'https://api.github.com/users/' + user + '/repos'
+
 user_agent = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)' 
 	          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36')
 header = {'User-Agent' : user_agent}
@@ -11,8 +10,15 @@ header = {'User-Agent' : user_agent}
 payload = {'client_id': 'e5f977a68ace627a1760', 'client_secret': 'aa9e159b082180e875a96d68d84e26324771e5b4'}
 
 def get_json(url):
+
+	# check if the request exceeds rate limit 
+	r = requests.get('https://api.github.com/rate_limit', headers = header, params = payload)
+	if r.headers['X-RateLimit-Remaining'] == 0:
+		print('Exceeds rate limit.')
+		return (False)	
+	
 	try:
-		respnose = requests.get(url, headers = header, params = payload)
+		response = requests.get(url, headers = header, params = payload)
 
 	except requests.exceptions.ConnectionError as e:	
 		print('We failed to reach a server.', e)
@@ -32,32 +38,41 @@ def get_json(url):
 
 	else:
 		try:
-			respnose.raise_for_status()
+			response.raise_for_status()
 		except requests.exceptions.HTTPError as e:
-			print('The server couldn\'t fulfill the request.', e)
+			print('Bad request:', url, response.headers['Status'])
 			return(False)		
 		else:	
-			return(respnose.json())
+			return(response.json())
+
+dataset_path = '/Users/DerekChiang/Documents/Github repo/social_influencer/Java_dataset/6273/'
+fname = dataset_path + 'top30-award.txt'
+# try:
+# 	fhand = open(fname, 'r')
+# except:
+# 	print('Could not open file', fname)
+# else:	
+# 	top30 = list()
+# 	followers = list()
+# 	api_url = 'https://api.github.com/users/'
+# 	for line in fhand:
+# 		user = line.rstrip()
+# 		top30.append(user)
+
+# 		user_url = api_url + user 
+# 		user_dict = get_json(user_url)
+# 		followers.append(user_dict['followers'])
+# 	# for n in zip(top30, followers):
+# 	# 	print(n)	
+# 	indices = list(range(len(top30)))
+# 	indices.sort(key = lambda x: followers[x], reverse = True)
+
+# 	with open(dataset_path + 'top30-follower.txt', 'w') as outfile:
+# 		for i in indices:
+# 			outfile.write(top30[i] + '\n')
+# 			print(followers[i])
+# 	print('Saved')		
 
 
-repo_list = get_json(user_url)
-if not repo_list:
-	print('Please retry.')
-else:
-	print('User', user, 'has', len(repo_list), 'public repositories.')
-	python_repos = list()
-	for repo in repo_list:
-		if (repo['language'] == 'Python' and repo['fork'] == False):
-			python_repos.append(repo['url'])
-	print('Written in Python(fork repos are excluded):', len(python_repos))
-	for url in python_repos:
-		repo = get_json(url)
-		if not repo:
-			print('Can\'t find the repository.')
-		else:	
-			print('\nRepository name:', repo['full_name'])
-			print('Watch:', repo['subscribers_count'])
-			print('Star:', repo['stargazers_count'])	
-			print('Fork:', repo['forks_count'])	
 
 
