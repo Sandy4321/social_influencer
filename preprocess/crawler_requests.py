14447b0e14de386964a5d52e991eea5494dd6f3b
Derek Chiang <b00611007@gmail.com>
2018-01-03 11:17:22 +0800

    Adjust data file and scripts directory.

---
+++ preprocess/crawler_requests.py
@@ -0,0 +1,79 @@
+# encoding = 'utf-8'
+import requests
+
+
+user_agent = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)' 
+	          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36')
+header = {'User-Agent' : user_agent}
+
+#Increasing the unauthenticated rate limit for OAuth applications
+payload = {'client_id': 'e5f977a68ace627a1760', 'client_secret': 'aa9e159b082180e875a96d68d84e26324771e5b4'}
+
+def get_json(url):
+
+	# check if the request exceeds rate limit 
+	r = requests.get('https://api.github.com/rate_limit', headers = header, params = payload)
+	if r.headers['X-RateLimit-Remaining'] == 0:
+		print('Exceeds rate limit.')
+		return (False)	
+	
+	try:
+		response = requests.get(url, headers = header, params = payload)
+
+	except requests.exceptions.ConnectionError as e:	
+		print('We failed to reach a server.', e)
+		return(False)
+
+	except requests.exceptions.Timeout:
+		print('The request has timed out.')
+		return(False)
+
+	except requests.exceptions.TooManyRedirects:
+		print('The request exceeds the configured number of maximum redirections.')
+		return(False)
+	
+	except requests.exceptions.RequestException as e:
+		print('Other exceptions.', e)
+		return(False)
+
+	else:
+		try:
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as e:
+			print('Bad request:', url, response.headers['Status'])
+			return(False)		
+		else:	
+			return(response.json())
+
+# dataset_path = '/Users/DerekChiang/Documents/Github repo/social_influencer/Java_dataset/6273/'
+# fname = dataset_path + 'top30-award.txt'
+# try:
+# 	fhand = open(fname, 'r')
+# except:
+# 	print('Could not open file', fname)
+# else:	
+# 	top30 = list()
+# 	followers = list()
+# 	api_url = 'https://api.github.com/users/'
+# 	for line in fhand:
+# 		user = line.rstrip()
+# 		top30.append(user)
+
+# 		user_url = api_url + user 
+# 		user_dict = get_json(user_url)
+# 		followers.append(user_dict['followers'])
+# 	# for n in zip(top30, followers):
+# 	# 	print(n)	
+# 	indices = list(range(len(top30)))
+# 	indices.sort(key = lambda x: followers[x], reverse = True)
+
+# 	with open(dataset_path + 'top30-follower.txt', 'w') as outfile:
+# 		for i in indices:
+# 			outfile.write(top30[i] + '\n')
+# 			print(followers[i])
+# 	print('Saved')		
+url = 'https://api.github.com/repos/DerekChiangTW/social_influencer/commits'
+lists = get_json(url)
+print(len(lists))
+
+
---
+++ preprocess/evaluation.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+
+def spearman(x, y):
+    """
+    Calculates a Spearman rank-order correlation coefficient.
+
+    Parameters
+    ----------
+    x, y : input lists 
+           All the ranks in the lists should be distinct integers.
+
+    Returns
+    -------
+    rho : float 
+
+    """
+
+    # check inputs
+    if (not x or len(x) == 1 or len(x) != len(y)):
+        raise ValueError('Invalid input lists.')
+
+    n = len(x)
+    diff = 0.0
+    for i in range(n):
+        diff += (x[i] - y[i])**2	
+
+    n = len(x)
+    diff = 0.0
+    for i in range(n):
+        diff += (x[i] - y[i])**2	
+
+    return(1.0 - 6.0 * diff / (n * (n**2 - 1.0)))
+
+
+
+def kendall(x, y):
+    """
+    Calculates a Kendall rank-order correlation coefficient.
+
+    Parameters
+    ----------
+    x, y : input lists 
+           All the ranks in the lists should be distinct integers.
+
+    Returns
+    -------
+    tau : float 
+
+    """
+
+    # check inputs
+    if (not x or len(x) == 1 or len(x) != len(y)):
+        raise ValueError('Invalid input lists.')
+
+    n = len(x)	 
+    concordant = discordant = 0
+
+    sorted_order = np.argsort(x)  
+    new_x = np.array(x)[sorted_order] 	
+    new_y = np.array(y)[sorted_order] 
+
+    for i in range(n - 1):
+        count = 0
+        for j in range(i + 1, n):
+            if new_y[j] > new_y[i]:
+                count += 1 
+        concordant += count
+        discordant += n - (i + 1) - count
+
+    return((concordant - discordant) / (0.5 * n * (n - 1)))	
+
+
+
+def rank_of_list(lst):
+    """
+    Rank the list.
+
+    Parameters
+    ----------
+    lst : input list
+           
+    Returns
+    -------
+    rankList : a list
+
+    """
+	
+    # check input
+    if not lst:
+        raise ValueError('Empty input list.')
+
+    rankList = [0 for x in range(len(lst))]
+    indices = list(range(len(lst)))
+
+    # sort the position of elements
+    indices.sort(key = lambda x: lst[x], reverse = True)   
+
+    n = 1
+    for i in indices:
+        rankList[i] = n
+        n += 1
+
+    return(rankList)	
+
+
+
